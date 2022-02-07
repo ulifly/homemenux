cab7838eaa9aa9af65d5d9bef1d1e299dd06bb1d
Ulises Desentis <ulifly@hotmail.com>
2022-02-07 15:44:25 -0600

    ingredients deploy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_03_222434) do
+ActiveRecord::Schema.define(version: 2022_02_07_212725) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
