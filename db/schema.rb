dee50efe6e089557bc22112b10c2f3713b162c91
Ulises <ulifly@hotmail.com>
2022-02-11 15:57:24 -0600

    ingredients deployed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_11_190411) do
+ActiveRecord::Schema.define(version: 2022_02_11_213639) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.integer "plate_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["plate_id"], name: "index_ingredients_on_plate_id"
+  end
 
   create_table "plates", force: :cascade do |t|
     t.string "name"
@@ -33,4 +41,5 @@ ActiveRecord::Schema.define(version: 2022_02_11_190411) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "ingredients", "plates"
 end
