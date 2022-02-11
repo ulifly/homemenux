a07f5d0130da19e9a6f42007e408948cd2d94d8d
Ulises <ulifly@hotmail.com>
2022-02-11 16:36:54 -0600

    views update

--- app/models/plate.rb
+++ app/models/plate.rb
@@ -1,4 +1,5 @@
 class Plate < ApplicationRecord
+  has_many :ingredients
   mount_uploader :image, ImageUploader
   serialize :images, JSON # If you use SQLite, add this line.check CarrierWave documentation
   belongs_to :user, optional: true
