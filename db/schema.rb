f425e0fe2423f409a8526e2bea411f1750233746
Ulises <ulifly@hotmail.com>
2022-02-15 18:32:56 -0600

    switch db from sqlite to postgresqlite

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2022_02_11_213639) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
-    t.integer "plate_id", null: false
+    t.bigint "plate_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["plate_id"], name: "index_ingredients_on_plate_id"
